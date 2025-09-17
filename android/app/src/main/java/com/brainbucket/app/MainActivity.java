ccc834cd51407f170d668e6eb1fae054dbb586d3
kmkohl117-gif <kmkohl117@gmail.com>
2025-09-17 07:59:35 -0600

    Update MainActivity.java

--- android/app/src/main/java/com/brainbucket/app/MainActivity.java
+++ android/app/src/main/java/com/brainbucket/app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.brainbucket.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import androidx.core.view.WindowCompat;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Make system bars NOT overlap the WebView
+    WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+  }
+}
