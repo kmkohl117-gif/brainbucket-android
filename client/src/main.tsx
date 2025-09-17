c32654ac3aba261c5cd34bc889c4f2e17a5257d6
kmkohl117-gif <kmkohl117@gmail.com>
2025-09-17 07:02:43 -0600

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Prevent content from being jammed under the status bar
+import { StatusBar } from "@capacitor/status-bar";
+StatusBar.setOverlaysWebView({ overlay: false }).catch(() => {});
+
 createRoot(document.getElementById("root")!).render(<App />);
