779b90db8b590f95d1511d2128b59eafaedc6f50
kmkohl117-gif <kmkohl117@gmail.com>
2025-09-18 06:54:55 -0600

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+(window as any).__API_BASE_URL__ = 'https://2d6b8952-57b8-4a6c-a041-fd01d3d89cf1-00-1l33vlsqoz733.riker.replit.dev/';
+
 // Prevent content from being jammed under the status bar
 import { StatusBar } from "@capacitor/status-bar";
 StatusBar.setOverlaysWebView({ overlay: false }).catch(() => {});
