59ea97c49f15cafcfadbc3293bf33ea484fba965
kmkohl117-gif <kmkohl117@gmail.com>
2025-10-19 12:07:13 -0600

    Updated SearchView.tsx

--- src/components/SearchView.tsx
+++ src/components/SearchView.tsx
@@ -61,7 +61,7 @@ export function SearchView() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col pb-20">
+    <div className="min-h-screen bg-gray-50 flex flex-col pb-20 pt-12">
       {/* Header */}
       <div className="bg-white border-b border-gray-200 px-4 py-4">
         <div className="flex items-center space-x-3 mb-3">
