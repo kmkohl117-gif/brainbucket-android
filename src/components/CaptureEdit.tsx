eea58050b783e596e548f05129a38e78c31191bd
kmkohl117-gif <kmkohl117@gmail.com>
2025-10-19 20:53:25 -0600

    Updated CaptureEdit.tsx

--- src/components/CaptureEdit.tsx
+++ src/components/CaptureEdit.tsx
@@ -112,7 +112,7 @@ export function CaptureEdit() {
   };
 
   return (
-<div className="min-h-screen bg-gray-50 flex flex-col pb-20 pt-12">
+<div className="min-h-screen bg-gray-50 flex flex-col pb-20 safe-top">
   {/* Header */}
       <div className="bg-white border-b border-gray-200 px-4 py-4">
         <div className="flex items-center justify-between">
