0615261436ff2af04d3febc7878c90252e29b5b8
kmkohl117-gif <kmkohl117@gmail.com>
2025-10-19 20:52:53 -0600

    Updated BucketDetail.tsx

--- src/components/BucketDetail.tsx
+++ src/components/BucketDetail.tsx
@@ -69,7 +69,7 @@ export function BucketDetail() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col pb-20 pt-[calc(env(safe-area-inset-top,0px)+48px)]">
+    <div className="min-h-screen bg-gray-50 flex flex-col pb-20 safe-top">
       {/* Header */}
       <div className="bg-white border-b border-gray-200 px-4 py-4">
         <div className="flex items-center justify-between">
