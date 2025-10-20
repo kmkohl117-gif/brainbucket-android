58e81de2e050704877df549561137efb92942d53
kmkohl117-gif <kmkohl117@gmail.com>
2025-10-19 22:12:33 -0600

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // 👈 this is the critical fix for online hosting
+  base: './', // 👈 this is the critical fix for online hosting
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
